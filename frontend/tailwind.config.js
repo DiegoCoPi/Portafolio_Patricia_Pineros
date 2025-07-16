ee8b2791f6df28302d582fa0d301b55a3e5c2f3b
FT51Grupo02 <pfgrupo2ft51@gmail.com>
2025-07-15 23:46:29 -0500

    Avance 15-07-2025

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,14 @@
+module.export={
+    content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    ],
+    theme:{
+        extend:{
+            backgroundImage:{
+                "background_img":"url('./public/contador_2.jpg')"
+            }
+        }
+    },
+    plugins: [],
+}
