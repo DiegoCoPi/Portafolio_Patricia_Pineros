c1b9d0da9df26a4f7c4813bfd969330b0d5be7cd
FT51Grupo02 <pfgrupo2ft51@gmail.com>
2025-07-16 22:39:47 -0500

    Avance 16-07-2025

--- frontend/src/components/footer/footer.tsx
+++ frontend/src/components/footer/footer.tsx
@@ -3,19 +3,25 @@ import Image from "next/image";
 
 export const Footer:React.FC=()=>{
     return (
-        <footer>
+        <footer className="flex-buttom-0 bg-indigo-800 bg-opacity-30">
             <div>
                 <h2>Sandra Patricia Piñeros</h2>
             </div>
-            <div className="Column-3">
-                <Image src="/telefono.svg" alt="telefono" height={30} width={30} />
-                <p>+57 3187119386</p>
-           
-                <Image src="/whatsapp.svg" alt="whatsapp" height={30} width={30} />
-                <p>+57 3187119386</p>
-          
-                <Image src="/email.svg" alt="email" height={30} width={30} />
-                <p>patriciapineros@gmail.com</p>
+            <div className="column-2">
+                <div className="flex justify-evenly items-center flex-wrap gap-4 mt-2">
+                    <div className="flex items-center gap-2">
+                        <Image src="/telefono.svg" alt="telefono" height={30} width={30} />
+                        <p>+57 3187119386</p>
+                    </div>
+                    <div className="flex items-center gap-2">
+                        <Image src="/whatsapp.svg" alt="whatsapp" height={30} width={30} />
+                        <p>+57 3187119386</p>
+                    </div>
+                    <div className="flex items-center gap-2">
+                        <Image src="/email.svg" alt="email" height={30} width={30} />
+                        <p>patriciapineros@gmail.com</p>
+                    </div>
+                </div>
             </div>
         </footer>
     )
