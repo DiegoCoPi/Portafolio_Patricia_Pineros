1468750f1af6a57ad176e641a26feaf10b594297
FT51Grupo02 <pfgrupo2ft51@gmail.com>
2025-07-24 00:31:34 -0500

    Avance 23-07-2025

--- frontend/src/components/form_indicator/form_indicators.tsx
+++ frontend/src/components/form_indicator/form_indicators.tsx
@@ -1,48 +1,51 @@
-import React, { useState } from "react";
-import { formInterface } from "./interface_form";
+"use client";
+import {useState}  from "react"
+import { formInterface } from "./interface_form"
+//import Butons from "../buttons/send_buton"
 
-export const MyForm=()=>{
+
+export const DecForm=()=>{
+    
+    
     const[formData, setFormData]=useState<formInterface>({
-        name:"",
+        name: "",
         lastname:"",
-        document_type:"",
-        document_number:1234567890,
-    });
+        document_number:0,
+    })
 
-    return(
-        <form>
-            <input
-                type="text"
-                placeholder="Ingrese su(s) nombre(s)"
-                value={formData.name}    
-                onChange={(e)=>setFormData({...formData, name:e.target.value})}
-            />
-
-            <input
-                type="text"
-                placeholder="Ingrese su(s) apellido(s)"
-                value={formData.lastname}
-                onChange={(e)=>setFormData({...formData, lastname:e.target.value})}
-            />
-            <label htmlFor="documentType">Tipo de documento</label>
-            <select
-                id="documentType"
-                value={formData.document_type}
-                onChange={(e)=>setFormData({...formData, document_type:e.target.value as formInterface["document_type"]})}
-            >
-                <option value="">Seleccione el tipo de documento</option>
-                <option value="Cedula cuidadana">Cédula cuidadana</option>
-                <option value="Cedula Extranjera">Cédula Extranjera</option>
-            </select>
-            <input
-                type="number"
-                placeholder="Ingrese numero de documento sin comas ni espacio ej 1234567890"
-                value={formData.document_number}
-                onChange={(e)=>setFormData({...formData, document_number: parseInt(e.target.value)})}
-            />
-
-        </form>
+    
+    return (
+       <form className="space-y-3">
+           <div className="flex items-left gap-6">
+                <h2>Nombre(s)</h2>
+                <input
+                    type="text"
+                    className="bg-gray-200 w-50"
+                    placeholder="Ingresa sus(s) nombre(s)"
+                    value={formData.name}
+                    onChange={(e)=>{setFormData({...formData, name:e.target.value})}}
+                />
+            </div>
+            <div className="flex items-left gap-6">
+                <h2>Apellido(s)</h2>
+                <input
+                    type="text"
+                    className="bg-gray-200 w-50"
+                    placeholder="Ingresa su(s) apellido(s)"
+                    value={formData.lastname}
+                    onChange={(e)=>{setFormData({...formData, lastname:e.target.value})}}
+                />
+            </div>
+            <div className="flex items-left gap-11.5">
+                <h2>N° C.C.</h2>
+                <input
+                    type="number"
+                    className="bg-gray-200 w-32"
+                    placeholder="Ingrese el numero del documento sin puntos ni espacios"
+                    value={formData.document_number}
+                    onChange={(e)=>{setFormData({...formData, document_number:Number(e.target.value)})}}
+                />
+            </div>
+       </form>
     )
-
 }
-
--- frontend/src/components/form_indicator/interface_form.ts
+++ frontend/src/components/form_indicator/interface_form.ts
@@ -2,6 +2,5 @@
 export interface formInterface{
    name:string;
    lastname:string;
-   document_type: ''|'cedula_cuidadana'|'cedula_extranjera';
    document_number: number;
 }
