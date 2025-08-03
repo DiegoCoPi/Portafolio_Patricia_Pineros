b07bd6a181783dfca81451c9f1b4fed91738f944
FT51Grupo02 <pfgrupo2ft51@gmail.com>
2025-08-03 12:18:57 -0500

    Avance 03-08-2025

--- backend/src/database/database.providers.ts
+++ backend/src/database/database.providers.ts
@@ -1,2 +1,17 @@
-import * as mongoose from  'moongose';
+/* eslint-disable prettier/prettier */
+import * as mongoose from 'mongoose';
 
+export const dataBase_Providers = [{
+    provide: 'DATABASE_CONNECTION',
+    useFactory:async():Promise<typeof mongoose>=>{
+        try{
+            const connection = await mongoose.connect('mongodb://localhost/nest');
+            console.log('DataBase conected sucessfully');
+            return connection;
+        }
+        catch(error){
+            console.log('DataBase_Error: ',error)
+            throw error
+        }
+    }
+}];
